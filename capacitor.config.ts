73bdec3f57a9f107e6d072ef2c5f9e623635ca8a
Scalisi <dario.scalisi@sap.com>
2025-12-07 14:59:38 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Wasserbedarf_einer_Planze',
+  webDir: 'www'
+};
+
+export default config;
